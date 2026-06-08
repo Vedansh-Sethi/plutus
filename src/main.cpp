f8d4ae17b8cdd3e872b8aac797d435579af221a2
Vedansh Sethi <vedansh25sethi@gmail.com>
2026-06-08 17:28:43 +0530

    testing memory pool

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,38 @@
+#include "utils/mempool.hpp"
 #include "utils/thread.hpp"
 #include <iostream>
 #include <thread>
 
-auto dummyFunction(int a, int b, bool sleep)
+struct MyStruct
 {
-    std::cout << "dummyFunction(" << a << "," << b << ")" << std::endl;
-    std::cout << "dummyFunction output = " << a + b << std::endl;
-    if (sleep)
-    {
-        std::cout << "dummyFunction sleeping..." << std::endl;
-        using namespace std::literals::chrono_literals;
-        std::this_thread::sleep_for(5s);
-    }
-    std::cout << "dummyFunction done." << std::endl;
-}
+    int d_[3];
+};
 
 int main(int, char **)
 {
-    auto t1 = createAndStartThread(-1, "dummyFunction1", dummyFunction, 12, 21,
-                                   false);
-    auto t2 =createAndStartThread(1, "dummyFunction2", dummyFunction, 15, 51, true);
-    std::cout << "main waiting for thread to be done." << std::endl;
-    t1->join();
-    t2->join();
-    std::cout << "main exiting." << std::endl;
+    MemPool<double> prim_pool(50);
+    MemPool<MyStruct> struct_pool(50);
+    for (auto i = 0; i < 50; i++)
+    {
+        auto p_ret = prim_pool.allocate(double(i));
+        auto s_ret = struct_pool.allocate(MyStruct{i, i + 1, i + 2});
+        std::cout << "prim_elem: " << *p_ret << " allocated at :" << p_ret
+                  << std::endl;
+        std::cout << "struct elem: " << s_ret->d_[0] << "," << s_ret->d_[1]
+                  << "," << s_ret->d_[2] << " allocated at :" << s_ret
+                  << std::endl;
+        if (i % 5 == 0)
+        {
+            std::cout << std::endl;
+            std::cout << "deallocating prim elem: " << *p_ret
+                      << " from : " << p_ret << std::endl;
+            std::cout << "deallocating struct elem: " << s_ret->d_[0] << ","
+                      << s_ret->d_[1] << "," << s_ret->d_[2]
+                      << " from : " << s_ret << std::endl;
+            prim_pool.deallocate(p_ret);
+            struct_pool.deallocate(s_ret);
+            std::cout << std::endl;
+        }
+    }
     return 0;
 }
